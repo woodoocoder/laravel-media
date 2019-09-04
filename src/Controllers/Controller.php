b6d066b900cfbdaa1b76d48b9fa87eb8105a9f2a
balaushko <dima19912012@gmail.com>
2019-09-04 22:21:56 +0300

    namespace fix

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Woodoocoder\LaravelMedia;
+namespace Woodoocoder\LaravelMedia\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
